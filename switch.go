49deb3b502adaf30caf25ef4455f4a6277a8833f
HappyTrain <93471494+HappyTrain@users.noreply.github.com>
2025-06-26 10:55:51 +0800

    Add files via upload

---
+++ switch.go
@@ -0,0 +1,158 @@
+// Copyright 2014 Team 254. All Rights Reserved.
+// Author: pat@patfairbank.com (Patrick Fairbank)
+//
+// Methods for configuring a Cisco Switch 3500-series switch for team VLANs.
+
+package network
+
+import (
+	"bufio"
+	"bytes"
+	"fmt"
+	"github.com/Team254/cheesy-arena/model"
+	"net"
+	"sync"
+	"time"
+)
+
+const (
+	switchConfigBackoffDurationSec = 5
+	switchConfigPauseDurationSec   = 2
+	switchTeamGatewayAddress       = 4
+	switchTelnetPort               = 23
+)
+
+const (
+	red1Vlan  = 10
+	red2Vlan  = 20
+	red3Vlan  = 30
+	blue1Vlan = 40
+	blue2Vlan = 50
+	blue3Vlan = 60
+)
+
+type Switch struct {
+	address               string
+	port                  int
+	password              string
+	mutex                 sync.Mutex
+	configBackoffDuration time.Duration
+	configPauseDuration   time.Duration
+	Status                string
+}
+
+var ServerIpAddress = "10.0.100.5" // The DS will try to connect to this address only.
+
+func NewSwitch(address, password string) *Switch {
+	return &Switch{
+		address:               address,
+		port:                  switchTelnetPort,
+		password:              password,
+		configBackoffDuration: switchConfigBackoffDurationSec * time.Second,
+		configPauseDuration:   switchConfigPauseDurationSec * time.Second,
+		Status:                "UNKNOWN",
+	}
+}
+
+// Sets up wired networks for the given set of teams.
+func (sw *Switch) ConfigureTeamEthernet(teams [6]*model.Team) error {
+	// Make sure multiple configurations aren't being set at the same time.
+	sw.mutex.Lock()
+	defer sw.mutex.Unlock()
+	sw.Status = "CONFIGURING"
+
+	// Remove old team VLANs to reset the switch state.
+	removeTeamVlansCommand := ""
+	for vlan := 10; vlan <= 60; vlan += 10 {
+		removeTeamVlansCommand += fmt.Sprintf(
+			"delete ipif vlan%d\n"+
+				"delete dhcp pool vlan%d\n", vlan, vlan,
+		)
+	}
+	_, err := sw.runConfigCommand(removeTeamVlansCommand)
+	if err != nil {
+		sw.Status = "ERROR"
+		return err
+	}
+	time.Sleep(sw.configPauseDuration)
+
+	// Create the new team VLANs.
+	addTeamVlansCommand := ""
+	addTeamVlan := func(team *model.Team, vlan int) {
+		if team == nil {
+			return
+		}
+		teamPartialIp := fmt.Sprintf("%d.%d", team.Id/100, team.Id%100)
+		addTeamVlansCommand += fmt.Sprintf(
+			"config ipif vlan%d ipaddress 10.%s.%d/24\n"+
+				"enable dhcp_local_server\n"+
+				"create dhcp pool vlan%d\n"+
+				"config dhcp pool vlan%d add iprange 10.%s.20 10.%s.199\n"+
+				"config dhcp pool vlan%d gateway ip 10.%s.%d\n"+
+				"config dhcp pool vlan%d dns ip 8.8.8.8\n"
+			vlan, teamPartialIp, switchTeamGatewayAddress,
+			vlan,
+			vlan, teamPartialIp, teamPartialIp,
+			vlan, teamPartialIp, switchTeamGatewayAddress,
+			vlan,
+		)
+	}
+	addTeamVlan(teams[0], red1Vlan)
+	addTeamVlan(teams[1], red2Vlan)
+	addTeamVlan(teams[2], red3Vlan)
+	addTeamVlan(teams[3], blue1Vlan)
+	addTeamVlan(teams[4], blue2Vlan)
+	addTeamVlan(teams[5], blue3Vlan)
+	if len(addTeamVlansCommand) > 0 {
+		_, err = sw.runConfigCommand(addTeamVlansCommand)
+		if err != nil {
+			sw.Status = "ERROR"
+			return err
+		}
+	}
+
+	// Give some time for the configuration to take before another one can be attempted.
+	time.Sleep(sw.configBackoffDuration)
+
+	sw.Status = "ACTIVE"
+	return nil
+}
+
+// Logs into the switch via Telnet and runs the given command in user exec mode. Reads the output and
+// returns it as a string.
+func (sw *Switch) runCommand(command string) (string, error) {
+	// Open a Telnet connection to the switch.
+	conn, err := net.Dial("tcp", fmt.Sprintf("%s:%d", sw.address, sw.port))
+	if err != nil {
+		return "", err
+	}
+	defer conn.Close()
+
+	// Login to the AP, send the command, and log out all at once.
+	writer := bufio.NewWriter(conn)
+	_, err = writer.WriteString(fmt.Sprintf("%s\n%s\n", sw.password,
+		command))
+	/*_, err = writer.WriteString(fmt.Sprintf("%s\nenable\n%s\nterminal length 0\n%sexit\n",
+    sw.password, sw.password, command))  Cisco才需要的指令*/
+	if err != nil {
+		return "", err
+	}
+	err = writer.Flush()
+	if err != nil {
+		return "", err
+	}
+
+	// Read the response.
+	var reader bytes.Buffer
+	_, err = reader.ReadFrom(conn)
+	if err != nil {
+		return "", err
+	}
+	return reader.String(), nil
+}
+
+// Logs into the switch via Telnet and runs the given command in global configuration mode. Reads the output
+// and returns it as a string.
+func (sw *Switch) runConfigCommand(command string) (string, error) {
+	return sw.runCommand(command)//Cisco才需要的指令(fmt.Sprintf("config terminal\n%send\n\n", command))
+}
